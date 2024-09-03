9b11dff3f693058ee6058c961adf2e291aaa91fe
RememberThePorter <connor.james.hanson@gmail.com>
2024-09-03 16:19:40 -0700

    Initial commit

---
+++ src/main/java/net/creepersneedhugs/firstproject/util/ModLootTableModifiers.java
@@ -0,0 +1,26 @@
+package net.creepersneedhugs.firstproject.util;
+
+import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
+import net.minecraft.entity.EntityType;
+import net.minecraft.item.Items;
+import net.minecraft.loot.LootPool;
+import net.minecraft.loot.condition.RandomChanceLootCondition;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.function.SetCountLootFunction;
+import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
+import net.minecraft.loot.provider.number.UniformLootNumberProvider;
+
+public class ModLootTableModifiers {
+    public static void modifyLootTables() {
+        LootTableEvents.MODIFY.register((key, tableBuilder, source) -> {
+            if(source.isBuiltin() && (EntityType.SHEEP.getLootTableId().equals(key))) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(0.5f)) //50% chance drop
+                        .with(ItemEntry.builder(Items.EGG))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f,1.0f)).build());
+                tableBuilder.pool(poolBuilder.build());
+            }
+        });
+    }
+}
