42e089fcfee8c22de22a3923fbbc88beaa9ef657
RememberThePorter <connor.james.hanson@gmail.com>
2024-09-03 20:14:24 -0700

    Fixing tin stuff

--- src/main/java/net/creepersneedhugs/firstproject/FirstProject.java
+++ src/main/java/net/creepersneedhugs/firstproject/FirstProject.java
@@ -1,5 +1,6 @@
 package net.creepersneedhugs.firstproject;
 
+import net.creepersneedhugs.firstproject.content.registry.ModBlocks;
 import net.creepersneedhugs.firstproject.content.registry.ModItems;
 import net.fabricmc.api.ModInitializer;
 
@@ -9,13 +10,12 @@ import org.slf4j.LoggerFactory;
 public class FirstProject implements ModInitializer {
 	public static final String MOD_ID = "firstproject";
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
+
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
+		ModBlocks.initialize();
 		ModItems.initialize();
 	}
 }
