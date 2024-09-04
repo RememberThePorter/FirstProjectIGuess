847a8dd833c179ab9968da32340eafa416dbfddb
RememberThePorter <connor.james.hanson@gmail.com>
2024-09-03 20:45:38 -0700

    More tin fixes (maybe)

--- src/main/java/net/creepersneedhugs/firstproject/FirstProject.java
+++ src/main/java/net/creepersneedhugs/firstproject/FirstProject.java
@@ -4,18 +4,27 @@ import net.creepersneedhugs.firstproject.content.registry.ModBlocks;
 import net.creepersneedhugs.firstproject.content.registry.ModItems;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.feature.PlacedFeature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class FirstProject implements ModInitializer {
 	public static final String MOD_ID = "firstproject";
 
-
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	public static final RegistryKey<PlacedFeature> TIN_ORE_PLACED_KEY = RegistryKey.of(RegistryKeys.PLACED_FEATURE, new Identifier("tutorial","ore_custom"));
 
 	@Override
 	public void onInitialize() {
 		ModBlocks.initialize();
 		ModItems.initialize();
+		BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, TIN_ORE_PLACED_KEY);
 	}
 }
