f5df2499c6330334eeb6679c53126f56c675f7a3
RememberThePorter <connor.james.hanson@gmail.com>
2024-09-05 18:49:02 -0700

    Fixed all the broken stuff.

--- src/main/java/net/creepersneedhugs/firstproject/content/registry/ModBlocks.java
+++ src/main/java/net/creepersneedhugs/firstproject/content/registry/ModBlocks.java
@@ -4,18 +4,26 @@ import net.creepersneedhugs.firstproject.FirstProject;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 
 public class ModBlocks {
 
     public static final Block DEEPSLATE_TIN_ORE = register(
-            new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.DEEPSLATE).strength(4.5f)),
+            new ExperienceDroppingBlock(UniformIntProvider.create(2, 5), AbstractBlock.Settings.create().sounds(BlockSoundGroup.STONE).strength(4.5f)),
             "deepslate_tin_ore",
+            true
+    );
+
+    public static final Block RAW_TIN_BLOCK = register(
+            new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.STONE).strength(5.0f)),
+            "raw_tin_block",
             true
     );
 
@@ -26,9 +34,9 @@ public class ModBlocks {
     );
 
     public static final Block TIN_ORE = register(
-            new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.STONE).strength(3.0f)),
-            "tin_ore",
-            true
+        new ExperienceDroppingBlock(UniformIntProvider.create(2, 5), AbstractBlock.Settings.create().sounds(BlockSoundGroup.STONE).strength(3.0f)),
+        "tin_ore",
+        true
     );
 
     public static Block register(Block block, String id, boolean shouldRegisterItem) {
@@ -43,6 +51,7 @@ public class ModBlocks {
     public static void initialize() {
         ItemGroupEvents.modifyEntriesEvent(ModItems.CUSTOM_ITEM_GROUP_KEY).register(itemGroup -> {
             itemGroup.add(ModBlocks.DEEPSLATE_TIN_ORE);
+            itemGroup.add(ModBlocks.RAW_TIN_BLOCK);
             itemGroup.add(ModBlocks.TIN_BLOCK);
             itemGroup.add(ModBlocks.TIN_ORE);
         });
